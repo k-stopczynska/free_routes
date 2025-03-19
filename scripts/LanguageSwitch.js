f6ab170ccc4bb5e678c819bfeb736a38559d724d
k.stopczynska <k.stopczynska@gmail.com>
2025-03-19 19:58:39 +0100

    lang switch

---
+++ scripts/LanguageSwitch.js
@@ -0,0 +1,52 @@
+import i18next from 'i18next';
+
+class LanguageSwitch {
+
+
+    constructor() {
+        initializeI18next();
+    }
+    
+    setDefaultLang() {
+   const userLang = navigator.language || navigator.userLanguage; 
+        const defaultLang = userLang.split('-')[0];
+        i18next.changeLanguage(defaultLang, () => {
+        render();  
+});
+    }
+    async initializeI18next() {
+ 
+    const en = await fetch('/locales/en.json').then(response => response.json());
+    const pl = await fetch('/locales/pl.json').then(response => response.json());
+
+    i18next.init({
+        lng: 'en',
+        resources: {
+            en: { translation: en },
+            pl: { translation: pl }
+        }
+    }, function (err, t) {
+        if (err) console.error('Error loading translations:', err);
+        render();
+    });
+}
+
+render() {
+    document.getElementById('saveButton').innerText = i18next.t('save');
+    document.getElementById("savedRoutes").innerText = i18next.t('savedRoutes'); 
+    document.getElementById("resetButton").innerText = i18next.t('resetMap');
+    document.getElementById("undoButton").innerText = i18next.t('undo');
+    document.getElementById("gpxUpload").innerText = i18next.t('uploadGPX');
+    document.getElementById("exportButton").innerText = i18next.t('exportGPX');
+    document.getElementById('distance').innerText = i18next.t('distance');
+}
+
+addListener() {
+        document.getElementById('languageSelector').addEventListener('change', function(e) {
+    const selectedLanguage = e.target.value;
+    i18next.changeLanguage(selectedLanguage, () => {
+        render();
+    });
+});
+    } 
+}
--- scripts/utils.js
+++ scripts/utils.js
@@ -12,38 +12,3 @@ export const calculateDistance = (points) => {
     renderDistance(distance);
     return distance;
 };
-
-export const exportToGPX = (routePoints) => {
-    if (!Array.isArray(routePoints)) {
-        console.error("Expected an array of route points.");
-        alert("Trasa jest niepoprawna. Spróbuj ponownie.");
-        return;
-    }
-    if (routePoints.length < 2) {
-        alert("Trasa jest za krótka do eksportu!");
-        return;
-    }
-
-    let gpxData = `<?xml version="1.0" encoding="UTF-8"?>
-<gpx version="1.1" creator="LeafletJS">
-  <trk>
-    <name>Eksportowana Trasa</name>
-    <trkseg>`;
-
-    routePoints.forEach(pt => {
-        if (pt && pt.lat && pt.lng) {
-            gpxData += `<trkpt lat="${pt.lat}" lon="${pt.lng}"></trkpt>\n`;
-        }
-    });
-
-    gpxData += `</trkseg></trk></gpx>`;
-
-    let blob = new Blob([gpxData], { type: "application/gpx+xml" });
-
-    let a = document.createElement('a');
-    a.href = URL.createObjectURL(blob);
-    a.download = "route.gpx";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-};
