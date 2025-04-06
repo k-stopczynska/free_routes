6004b894a289a4bfe06bd8385acfbe446a7c7660
k.stopczynska <k.stopczynska@gmail.com>
2025-04-06 14:06:03 +0200

    infinite loop fix

--- src/scripts/RouteCreator.js
+++ src/scripts/RouteCreator.js
@@ -30,12 +30,13 @@ export class RouteCreator {
     }
 
     getUsersParameters() { 
-        this.targetDistance = document.getElementById('routeDistance').value;
-        this.direction = document.getElementById('routeDirection').value;   
+        this.targetDistance = +document.getElementById('routeDistance').value;
+        this.direction = +document.getElementById('routeDirection').value;   
         this.routeType = document.getElementById('routeType').value;
     }
 
     async fetchData(coordinates) {
+        console.log(coordinates)
         const data = {
             coordinates: coordinates,
             alternative_routes: { target_count: 3, weight_factor: 5, share_factor: 0.5 },
@@ -60,8 +61,7 @@ export class RouteCreator {
             body: JSON.stringify(data)
         });
             const response = await request.json();
-            console.log(response);
-            if (response) return response;
+            return response;
         } catch(error) {
             console.error('No route found in response.', error);
         }
@@ -93,50 +93,58 @@ export class RouteCreator {
             }).addTo(this.map);
     }
     
-    async generateRoute(){
+    async generateRoute() {
 
-        const position = await this.geolocation.getCurrentPosition(); 
+        const position = await this.geolocation.getCurrentPosition();
         const { latitude, longitude } = position.coords;
         let pointB = this.calculateDestination(latitude, longitude, 2000, this.direction);
             
-    while (!this.foundRoute) {
-        const coordinates = [
-            [longitude, latitude],
-            [pointB.longitude, pointB.latitude]
-        ];
-        const response = await this.fetchData(coordinates);
-        let routeData;
-        const dist = response.features[0].properties.summary.distance + response.features[1].properties.summary.distance;
-        if (dist && dist > 0) {
-            const routeLength = dist;
-            console.log(`Route length: ${routeLength} meters`);
-            console.log(Math.abs(routeLength - this.targetDistance))
-            if (Math.abs(routeLength - this.targetDistance) <= 1000) {
-                this.foundRoute = true;
-                routeData = response.features;
-                console.log('Valid route found:', response.features);
-            }
-            else if (routeLength < this.targetDistance) {
-                pointB = this.calculateDestination(latitude, longitude, dist + 300, this.direction);
-                console.log(`Route too short. Trying new Point B: ${pointB.latitude}, ${pointB.longitude}`);
-            }
-            else if (Math.abs(routeLength - this.targetDistance) >= 1500) {
-                this.direction += 30;
+        while (!this.foundRoute) {
+            const coordinates = [
+                [longitude, latitude],
+                [pointB.longitude, pointB.latitude]
+            ];
+            const response = await this.fetchData(coordinates);
+            let routeData;
+            if (response.features.length > 1) {
+                const dist = response.features[0].properties.summary.distance + response.features[1].properties.summary.distance;
+                if (dist && dist > 0) {
+                    const routeLength = dist;
+                    console.log(`Route length: ${routeLength} meters`);
+                    console.log(Math.abs(routeLength - this.targetDistance))
+                    if (Math.abs(routeLength - this.targetDistance) <= 1000) {
+                        this.foundRoute = true;
+                        routeData = response.features;
+                        console.log('Valid route found:', response.features);
+                    }
+                    else if (routeLength < this.targetDistance) {
+                        pointB = this.calculateDestination(latitude, longitude, dist + 100, this.direction);
+                        console.log(`Route too short. Trying new Point B: ${pointB.latitude}, ${pointB.longitude}`);
+                    }
+                    else if (Math.abs(routeLength - this.targetDistance) >= 1500) {
+                        this.direction += 20;
+                        console.log(this.direction);
+                        pointB = this.calculateDestination(latitude, longitude, 2000, this.direction);
+                        console.log(pointB.latitude, pointB.longitude);
+                        console.log(`Route too long. Trying new Point B with a different bearing: ${pointB.latitude}, ${pointB.longitude}`);
+                    }
+                }
+                else {
+                    console.error('No route found in response.');
+                    break;
+                }
+        
+            } else { 
+                this.direction += 20;
                 console.log(this.direction);
-                pointB = this.calculateDestination(latitude, longitude, dist, this.direction);
+                pointB = this.calculateDestination(latitude, longitude, 2100, this.direction);
                 console.log(pointB.latitude, pointB.longitude);
-                console.log(`Route too long. Trying new Point B with a different bearing: ${pointB.latitude}, ${pointB.longitude}`);
+                console.log(`insufficient data length. Trying new Point B with a different bearing and dist: ${pointB.latitude}, ${pointB.longitude}`);
             }
-        }
-         else {
-            console.error('No route found in response.');
-            break;
-        }
-            
-        if (routeData && routeData[0].geometry && routeData[1].geometry) {
-            this.drawRoute(routeData);
-        }
-        }
+            if (routeData && routeData[0].geometry && routeData[1].geometry) {
+                this.drawRoute(routeData);
+            }
+    }
     }
     
     addEventListeners() { 
